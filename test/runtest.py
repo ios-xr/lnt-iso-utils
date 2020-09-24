622a708d429b34c57235f1dc6029d0c29979dd67
Patrick Smears <psmears@cisco.com>
2020-09-24 16:59:17 +0100

    Use a context directory

--- test/runtest.py
+++ test/runtest.py
@@ -12,14 +12,18 @@ template = tenv.get_template("template.dockerfile")
 
 with tempfile.TemporaryDirectory() as tmpdir:
     dockerfile_dir = os.path.join(tmpdir, "dockerfiles")
+    os.mkdir(dockerfile_dir)
+
+    context_dir = os.path.join(tmpdir, "context")
+    os.mkdir(context_dir)
 
 
     for base in bases:
         base_df = '{}.dockerfile'.format(base)
-        base_dfpath = os.path.join(tmpdir, base_df)
+        base_dfpath = os.path.join(dockerfile_dir, base_df)
         with open(base_dfpath, "w") as f:
             f.write(template.render(base=base))
-        subprocess.run(['sudo', '/ecs/utils/container/bin/subuildah', 'bud', '-f', base_dfpath], check=True)
+        subprocess.run(['sudo', '/ecs/utils/container/bin/subuildah', 'bud', '-f', base_dfpath, context_dir], check=True)
 
 
 
