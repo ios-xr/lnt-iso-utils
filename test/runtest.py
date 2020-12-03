ec22a01047a9be867ee604f218ee5774994d5703
Patrick Smears <psmears@cisco.com>
2020-12-03 03:10:46 -0800

    Keep black happy

--- test/runtest.py
+++ test/runtest.py
@@ -7,28 +7,39 @@ import shutil
 import tempfile
 import subprocess
 
-buildah = ['sudo', '/ecs/utils/container/bin/subuildah']
-podman = ['sudo', '/ecs/utils/container/bin/supodman']
+buildah = ["sudo", "/ecs/utils/container/bin/subuildah"]
+podman = ["sudo", "/ecs/utils/container/bin/supodman"]
 
-imagename = 'xrscripttest'
+imagename = "xrscripttest"
 
-bases = {'debian': {'from': 'debian:9.13-slim'},
-         'centos': {'from': 'centos:7.0.1406'}}
+bases = {"debian": {"from": "debian:9.13-slim"}, "centos": {"from": "centos:7.0.1406"}}
 
-files = ['xr-image-extract-rpms', 'test/prep-{base}', 'packages-{base}',
-         'test/test-xr-image-extract-rpms']
+files = [
+    "xr-image-extract-rpms",
+    "test/prep-{base}",
+    "packages-{base}",
+    "test/test-xr-image-extract-rpms",
+]
 
-isos = ['/release/IOX/bin/7.2.1/8000-x64-7.2.1.iso',
-        '/release/IOX/bin/7.0.11/8000-x64-7.0.11.iso']
+isos = [
+    "/release/IOX/bin/7.2.1/8000-x64-7.2.1.iso",
+    "/release/IOX/bin/7.0.11/8000-x64-7.0.11.iso",
+]
 
 tenv = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
 template = tenv.get_template("template.dockerfile")
 
+
 def create_dockerfile(fname, base):
     with open(fname, "w") as f:
-        f.write(template.render(base=base, from_=base_info["from"], files=[os.path.basename(f.format(base=base)) for f in files],
-                                               isos=[os.path.basename(i) for i in isos])) 
-
+        f.write(
+            template.render(
+                base=base,
+                from_=base_info["from"],
+                files=[os.path.basename(f.format(base=base)) for f in files],
+                isos=[os.path.basename(i) for i in isos],
+            )
+        )
 
 
 with tempfile.TemporaryDirectory() as tmpdir:
@@ -38,23 +49,26 @@ with tempfile.TemporaryDirectory() as tmpdir:
     context_dir = os.path.join(tmpdir, "context")
     os.mkdir(context_dir)
 
-
     for base, base_info in bases.items():
-        base_df = '{}.dockerfile'.format(base)
+        base_df = "{}.dockerfile".format(base)
         base_dfpath = os.path.join(dockerfile_dir, base_df)
 
         for f in files:
             f_base = f.format(base=base)
-            shutil.copyfile(os.path.join('..', f_base), 
-                            os.path.join(context_dir, os.path.basename(f_base)))
-            shutil.copymode(os.path.join('..', f_base), 
-                            os.path.join(context_dir, os.path.basename(f_base)))
+            shutil.copyfile(
+                os.path.join("..", f_base),
+                os.path.join(context_dir, os.path.basename(f_base)),
+            )
+            shutil.copymode(
+                os.path.join("..", f_base),
+                os.path.join(context_dir, os.path.basename(f_base)),
+            )
         for i in isos:
             shutil.copyfile(i, os.path.join(context_dir, os.path.basename(i)))
 
         create_dockerfile(base_dfpath, base)
         os.system("grep '' '{}'".format(base_dfpath))
-        cmd = buildah + ['bud', '-t', imagename, '-f', base_dfpath, context_dir]
+        cmd = buildah + ["bud", "-t", imagename, "-f", base_dfpath, context_dir]
         res = subprocess.run(cmd, capture_output=True)
         sys.stderr.buffer.write(res.stderr)
         if res.returncode != 0:
@@ -63,9 +77,5 @@ with tempfile.TemporaryDirectory() as tmpdir:
         else:
             imageid = res.stdout
 
-        cmd = podman + ['run', imagename]
+        cmd = podman + ["run", imagename]
         subprocess.run(cmd)
-
-
-
-
